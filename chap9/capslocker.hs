fe9c902b0754448eadfa87455bc450495a10100a
diohabara <diohabara@gmail.com>
2020-09-21 23:59:30 +0900

    fix: install interact

--- chap9/capslocker.hs
+++ chap9/capslocker.hs
@@ -1,9 +1,7 @@
 import Control.Monad
 import Data.Char
 
-main = do
-    contents <- getContents
-    putStr (shortLinesOnly contents)
+main = interact shortLinesOnly
 
 shortLinesOnly :: String -> String
 shortLinesOnly = unlines . filter (\line -> length line < 10) . lines
